37732ad76068b06b050dcfba4d4be729841bee0d
endrdragon <daendrdragon@gmail.com>
2020-07-16 16:55:59 -0400

    implement /regen

--- UHCUtils/src/main/java/chip/uhc/util/App.java
+++ UHCUtils/src/main/java/chip/uhc/util/App.java
@@ -2,6 +2,7 @@ package chip.uhc.util;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.LinkedHashMap;
 import dev.jorel.commandapi.CommandAPICommand;
@@ -11,10 +12,10 @@ import dev.jorel.commandapi.CommandPermission;
 public class App extends JavaPlugin {
     @Override
     public void onLoad() {
+        // /dispatch, /cmd: runs a command, used to run any plugin command in /execute and functions
         LinkedHashMap<String, Argument> dispatchArgs = new LinkedHashMap<>();
         dispatchArgs.put("command", new GreedyStringArgument());
-        
-        // /dispatch, /cmd: runs a command, used to run any plugin command in /execute and functions
+
         new CommandAPICommand("dispatch")
             .withArguments(dispatchArgs)
             .withPermission(CommandPermission.OP)
@@ -22,6 +23,29 @@ public class App extends JavaPlugin {
             .executes((sender, args) -> {
                 String cmd = (String) args[0];
                 Bukkit.dispatchCommand(sender, cmd);
+            })
+            .register();
+
+        // /regen <seed>: regens dims game & game_nether with provided seed
+        LinkedHashMap<String, Argument> regenWorldArgs = new LinkedHashMap<>();
+        regenWorldArgs.put("seed", new GreedyStringArgument());
+
+        new CommandAPICommand("regen")
+            .withArguments(regenWorldArgs)
+            .withPermission(CommandPermission.OP)
+            .executes((sender, args) -> {
+                String seed = (String) args[0];
+                if (seed.contains(" ")) seed = "\"" + seed + "\"";
+                if (Bukkit.getWorld("game") != null && Bukkit.getWorld("game_nether") != null) {
+                    sender.sendMessage("Reloading dimensions...");
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mvregen game -s " + seed);
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mvconfirm");
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mvregen game_nether -s " + seed);
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mvconfirm");
+                    sender.sendMessage("Both dimensions have been regenerated successfully.");
+                } else {
+                    sender.sendMessage("Dimensions game and game_nether must both exist in order to run this command.");
+                }
             })
             .register();
     }
