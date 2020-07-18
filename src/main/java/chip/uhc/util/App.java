b34489d71bd32f349b5d3a8ffc1a961197318e1b
endrdragon <daendrdragon@gmail.com>
2020-07-18 11:17:46 -0400

    add /runfn, restructure readme

--- src/main/java/chip/uhc/util/App.java
+++ src/main/java/chip/uhc/util/App.java
@@ -1,15 +1,19 @@
 package chip.uhc.util;
 
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.entity.Player;
+//import org.bukkit.GameMode;
+//import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.CommandException;
 import org.bukkit.ChatColor;
+import org.bukkit.scoreboard.Score;
 import java.util.LinkedHashMap;
+import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.*;
+import dev.jorel.commandapi.arguments.ScoreHolderArgument.ScoreHolderType;
 import dev.jorel.commandapi.wrappers.IntegerRange;
+import dev.jorel.commandapi.wrappers.FunctionWrapper;
 import dev.jorel.commandapi.CommandPermission;
 
 public class App extends JavaPlugin {
@@ -47,7 +51,7 @@ public class App extends JavaPlugin {
                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mvconfirm");
                     sender.sendMessage(ChatColor.GREEN + "Both dimensions have been regenerated successfully.");
                 } else {
-                    sender.sendMessage(ChatColor.RED + "Dimensions game and game_nether must both exist in order to run this command.");
+                    CommandAPI.fail("Dimensions game and game_nether must both exist in order to run this command.");
                 }
             })
             .register();
@@ -90,7 +94,7 @@ public class App extends JavaPlugin {
                         Bukkit.dispatchCommand(sender, itercmd);
                     }
                 } catch (CommandException e) {
-                    sender.sendMessage(ChatColor.RED + "An error occurred while running the commands: " + e.getMessage());
+                    CommandAPI.fail("An error occurred while running the commands: " + e.getMessage());
                 }
             })
             .register();
@@ -114,6 +118,59 @@ public class App extends JavaPlugin {
                 } catch (CommandException e) {
                     sender.sendMessage(ChatColor.RED + "An error occurred while running the commands: " + e.getMessage());
                 }
+            })
+            .register();
+
+            // /runfn <fn> with <player> <objective> as <value> || /runfn <fn> with <player> <objective> as <player> <objective>
+            // this fn sets scoreboard entry to <value> / <player> <objective>, runs the function, and returns the value on said scoreboard entry.
+            // this simplifies syntax for functions with args and allows them to be used with /for
+            // uhhhh, if you want to use multiple arguments just curry I guess
+            LinkedHashMap<String, Argument> runfnArgs = new LinkedHashMap<>();
+            runfnArgs.put("function", new FunctionArgument());
+            runfnArgs.put("with", new LiteralArgument("with"));
+            runfnArgs.put("player", new ScoreHolderArgument(ScoreHolderType.SINGLE));
+            runfnArgs.put("objective", new ObjectiveArgument());
+            runfnArgs.put("as", new LiteralArgument("as"));
+            runfnArgs.put("value", new IntegerArgument());
+
+            LinkedHashMap<String, Argument> runfnEntryArgs = new LinkedHashMap<>();
+            runfnEntryArgs.put("function", new FunctionArgument());
+            runfnEntryArgs.put("with", new LiteralArgument("with"));
+            runfnEntryArgs.put("player", new ScoreHolderArgument(ScoreHolderType.SINGLE));
+            runfnEntryArgs.put("objective", new ObjectiveArgument());
+            runfnEntryArgs.put("as", new LiteralArgument("as"));
+            runfnEntryArgs.put("player value", new ScoreHolderArgument(ScoreHolderType.SINGLE));
+            runfnEntryArgs.put("objective value", new ObjectiveArgument());
+
+            new CommandAPICommand("runfunction")
+            .withArguments(runfnEntryArgs)
+            .withPermission(CommandPermission.OP)
+            .withAliases("runfn")
+            .executes((sender, args) -> {
+                FunctionWrapper[] fns = (FunctionWrapper[]) args[0];
+                String inputPl = (String) args[1];
+                String inputOb = (String) args[2];
+                String valuePl = (String) args[3];
+                String valueOb = (String) args[4];
+                
+                // get (pl obj) slot and set it to val, effectively acting as the arg
+                Score inputScore = Bukkit.getScoreboardManager().getMainScoreboard().getObjective(inputOb).getScore(inputPl);
+                int inputVal = Bukkit.getScoreboardManager().getMainScoreboard().getObjective(valueOb).getScore(valuePl).getScore();
+                
+                inputScore.setScore(inputVal);
+                // run fns after arg has been set
+                for (FunctionWrapper fn : fns) {
+                    fn.run();
+                }
+                // return score for use w/ execute store
+                try {
+                    int retVal = inputScore.getScore();
+                    sender.sendMessage("Function returned " + Integer.toString(retVal));
+                    return retVal; 
+                } catch (IllegalArgumentException | IllegalStateException e) {
+                    CommandAPI.fail("Entry no longer exists");
+                }
+                return 0;
             })
             .register();
         
